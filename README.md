0ee55bba583a5ef0d99546e295abe5ef4268439c
hendrash <hendrash@mail.gvsu.edu>
2020-10-15 14:42:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-router-matcher-spppcr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-router-matcher-spppcr)
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, UrlSegment } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { ProfileComponent } from './profile/profile.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        matcher: (url) => {
+          if (url.length === 1 && url[0].path.match(/^@[\w]+$/gm)) {
+            return {
+              consumed: url,
+              posParams: {
+                username: new UrlSegment(url[0].path.substr(1), {})
+              }
+            };
+          }
+
+          return null;
+        },
+        component: ProfileComponent
+      }
+    ])
+  ],
+  declarations: [AppComponent, HelloComponent, ProfileComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
